4997292afe616ff0a8f10365d4a2052f865f1543
Ivriel <gunawanivriel@gmail.com>
2025-12-18 20:18:01 +0700

    User login dan middleware

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,7 +1,8 @@
-   <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
   <div class="container">
     <a class="navbar-brand" href="/">WPU Blog</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -12,20 +13,46 @@
         <li class="nav-item">
           <a class="nav-link {{ ($active === "about") ? 'active' : '' }}" href="/about">About</a>
         </li>
-          <li class="nav-item">
+        <li class="nav-item">
           <a class="nav-link {{ ($active === "posts") ? 'active' : '' }}" href="/posts">Blog</a>
         </li>
-         </li>
-          <li class="nav-item">
+        </li>
+        <li class="nav-item">
           <a class="nav-link {{ ($active === "categories") ? 'active' : '' }}" href="/categories">Categories</a>
         </li>
       </ul>
-
-      <ul class="navbar-nav ms-auto">
-        <li class="nav-item">
-          <a href="/login" class="nav-link {{ ($active === "login") ? 'active' : '' }}"><i class="bi bi-box-arrow-in-right"></i> Login</a>
+      <ul class="navbar-nav ms-auto list-unstyled">
+      @auth
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Welcome back, {{ auth()->user()->name }}
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="/dashboard"><i class="bi bi-layout-text-sidebar-reverse"></i> My Dashboard</a></li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+            <li>
+              <form action="/logout" method="POST">
+                @csrf
+                <button type="submit" class="dropdown-item">
+                  <i class="bi bi-box-arrow-right"></i> Logout
+                </button>
+              </form>
+            </li>
+          </ul>
         </li>
+        </ul>
+      @else
+
+        <li class="nav-item">
+          <a href="/login" class="nav-link {{ ($active === "login") ? 'active' : '' }}"><i
+              class="bi bi-box-arrow-in-right"></i> Login</a>
+        </li>
+      @endauth
       </ul>
+
+
     </div>
   </div>
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
@@ -50,6 +51,11 @@ Route::get('/categories',function(){
     ]);
 });
 
-Route::get('/login',[LoginController::class,'index']);
-Route::get('/register',[RegisterController::class,'index']);
+Route::get('/login',[LoginController::class,'index'])->name('login')->middleware('guest'); // guest ini dari kernel.php. hanya bisa diakes oleh user yang belum login. dikasih name karena di file Authenticate.php ada redirect ke route named login
+Route::post('/login',[LoginController::class,'authenticate']);
+Route::post('/logout',[LoginController::class,'logout']);
+
+Route::get('/register',[RegisterController::class,'index'])->middleware('guest');
 Route::post('/register',[RegisterController::class,'store']);
+
+Route::get('/dashboard',[DashboardController::class,'index'])->middleware('auth'); // hanya bisa diakses yang udah login
